4f50c84a98b67c3afd1deefe09ed42b8f52cc357
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-06-07 23:09:21 +0200

    feat: update to 1.20 fix: chat ban now works for messages with the length of 1 word

--- src/main/java/io/github/commander07/shoutheheckup/mixin/ChatHudMixin.java
+++ src/main/java/io/github/commander07/shoutheheckup/mixin/ChatHudMixin.java
@@ -28,9 +28,8 @@ public abstract class ChatHudMixin {
     @Inject(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At("HEAD"), cancellable = true)
     private void addMessageMixin(@NotNull Text message, MessageSignatureData signature, int ticks, MessageIndicator indicator, boolean refresh, CallbackInfo ci) {
         String[] message_arr = message.getString().split(" ");
-        ShouTheHeckUp.LOGGER.info(Arrays.toString(message_arr));
-        if (Config.enabled && message_arr.length >= 3) {
-            if (message_arr[2].contains("shouts:")) {
+        if (Config.enabled) {
+            if (message_arr.length >= 3 && message_arr[2].contains("shouts:")) {
                 if (Config.ignoredPlayers.contains(message_arr[0]) || Config.hideLevel == 3 || Config.bannedWords.stream().anyMatch(message.getString()::contains)) {
                     switch (Config.hideLevel) {
                         case 3, 2 -> ci.cancel();
@@ -45,20 +44,18 @@ public abstract class ChatHudMixin {
                     }
                 }
             }
-            else {
-                if (Config.filterChat && Config.bannedWords.stream().anyMatch(message.getString()::contains)) {
-                    switch (Config.hideLevel) {
-                        case 3, 2 -> {
-                            ci.cancel();
-                        }
-                        case 1 -> {
-                            addMessage(Text.of("§7§o[Blocked message by " + message_arr[0] + "]").getWithStyle(Text.empty().getStyle().withHoverEvent(HoverEvent.Action.SHOW_TEXT.buildHoverEvent(message))).get(0), signature, this.client.inGameHud.getTicks(), indicator, false);
-                            ci.cancel();
-                        }
-                        case 0 -> {
-                            addMessage(Text.of("§7§o[Blocked message]").getWithStyle(Text.empty().getStyle().withHoverEvent(HoverEvent.Action.SHOW_TEXT.buildHoverEvent(message))).get(0), signature, this.client.inGameHud.getTicks(), indicator, false);
-                            ci.cancel();
-                        }
+            else if (Config.filterChat && Config.bannedWords.stream().anyMatch(message.getString()::contains)) {
+                switch (Config.hideLevel) {
+                    case 3, 2 -> {
+                        ci.cancel();
+                    }
+                    case 1 -> {
+                        addMessage(Text.of("§7§o[Blocked message by " + message_arr[0] + "]").getWithStyle(Text.empty().getStyle().withHoverEvent(HoverEvent.Action.SHOW_TEXT.buildHoverEvent(message))).get(0), signature, this.client.inGameHud.getTicks(), indicator, false);
+                        ci.cancel();
+                    }
+                    case 0 -> {
+                        addMessage(Text.of("§7§o[Blocked message]").getWithStyle(Text.empty().getStyle().withHoverEvent(HoverEvent.Action.SHOW_TEXT.buildHoverEvent(message))).get(0), signature, this.client.inGameHud.getTicks(), indicator, false);
+                        ci.cancel();
                     }
                 }
             }
